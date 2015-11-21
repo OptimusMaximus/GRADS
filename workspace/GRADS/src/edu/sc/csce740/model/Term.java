b26f745d42d1cad2d55df96b42996867ba4643a1
MERCHANT <merchane@l-3d22-17.engr.sc.edu>
2015-11-21 15:45:33 -0500

    finished Javadoc up work by Ibrahim and Maximus. Still need: ProgSummary, RequirementCheck, and Student Record

--- workspace/GRADS/src/edu/sc/csce740/model/Term.java
+++ workspace/GRADS/src/edu/sc/csce740/model/Term.java
@@ -12,21 +12,48 @@ package edu.sc.csce740.model;
  *
  */
 public class Term {
+	/**
+	 * Instance variable containing the semester of the term, either SPRING, FALL, or SUMMER
+	 */
 	private String semester;
+	/**
+	 * Instance variable containing the year of the term
+	 */
 	private int year;
 	
+	/**
+	 * Method to get the semester of the term as a string
+	 * @return the semester of the term: FALL, SPRING, or SUMMER
+	 */
 	public String getSemester() {
 		return semester;
 	}
+	/**
+	 * Method to set the term semester
+	 * @param semester the string name of the semester to set FALL, SPRING, SUMMER
+	 */
 	public void setSemester(String semester) {
 		this.semester = semester;
 	}
+	/**
+	 * Method to get the year of the semester
+	 * @return year the year of the term
+	 */
 	public int getYear() {
 		return year;
 	}
+	/**
+	 * Method to set the year of the term
+	 * @param year the year of the term 
+	 */
 	public void setYear(int year) {
 		this.year = year;
 	}
+	/**
+	 * Convenience method to set all fields of term at one time
+	 * @param semester the semester of the term
+	 * @param year the year of the term
+	 */
 	public void setTerm(String semester, int year){
 		this.semester = semester;
 		this.year = year;
